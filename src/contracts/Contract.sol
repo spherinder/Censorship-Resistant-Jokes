7c9363507d2bb8cf97f7f343ca79caeb74b79602
spherinder <spherinder@gmail.com>
2020-02-13 22:55:07 +0100

    msg

--- src/contracts/Contract.sol
+++ src/contracts/Contract.sol
@@ -4,23 +4,19 @@ contract Contract {
   
   mapping (bytes32 => uint8) public ratingsReceived;
   
-  bytes32[] public mList;
+  bytes32[] public jokeList;
 
-  /* This is the constructor which will be called once when you
-  deploy the contract to the blockchain. When we deploy the contract,
-  we will pass an array of ms for which users will give ratings
-  */
-  constructor (bytes32[] memory mNames) public {
-    mList = mNames;
+  constructor (bytes32[] memory jokes) public {
+    jokeList = jokes;
   }
 
-  // This function returns the total ratings a m has received so far
-  function totalVotesFor(bytes32 m) view public returns (uint8) {
-    return ratingsReceived[m];
+  // This function returns the total ratings so far
+  function totalVotesFor(bytes32 joke) view public returns (uint8) {
+    return ratingsReceived[joke];
   }
 
-  // This function increments the vote count for the specified m. Equivalent to upvoting
-  function voteForM(bytes32 m) public {
-    ratingsReceived[m] += 1;
+  // This function increments the vote count
+  function voteForM(bytes32 joke) public {
+    ratingsReceived[joke] += 1;
   }
 }
